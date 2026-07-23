0c65f15ab9e7e2f3120f4fda3a75d01c02c85cf9
akash-sharma8 <sharmaakash8218@gmail.com>
2026-07-23 21:32:34 +0530

    Time: 15 ms (55.03%) | Memory: 47.3 MB (91.77%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int n: nums){
+            map.put(n,map.getOrDefault(n,0)+1);
+        }
+         PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> map.get(b) - map.get(a));
+        for(int key : map.keySet()){
+            pq.offer(key);
+        }
+        int[] ans = new int[k];
+        for(int i = 0; i < k; i++){
+            ans[i] = pq.poll();
+        }
+        return ans;
+    }
+}
