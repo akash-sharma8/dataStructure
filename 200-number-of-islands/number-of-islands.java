286becf6c60850cbe5fbbe2cf52fe138289acfc2
akash-sharma8 <sharmaakash8218@gmail.com>
2026-08-18 15:32:26 +0530

    Time: 2 ms (99.86%) | Memory: 52.4 MB (33.30%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,30 +1,30 @@
 class Solution {
-    private int n;
-    private int m;
-
-    public int numIslands(char[][] grid) {
-        int count = 0;
-        n = grid.length;
-        if (n == 0)
-            return 0;
-        m = grid[0].length;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++)
-                if (grid[i][j] == '1') {
-                    DFSMarking(grid, i, j);
-                    ++count;
-                }
-        }
-        return count;
-    }
-
-    private void DFSMarking(char[][] grid, int i, int j) {
-        if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] != '1')
+    public void dfs(int i, int j, char[][] grid) {
+        if (i < 0 || i >= grid.length  || j < 0 || j >= grid[0].length || grid[i][j] == '0')
             return;
         grid[i][j] = '0';
-        DFSMarking(grid, i + 1, j);
-        DFSMarking(grid, i - 1, j);
-        DFSMarking(grid, i, j + 1);
-        DFSMarking(grid, i, j - 1);
+        dfs(i - 1, j, grid);
+        dfs(i + 1, j, grid);
+        dfs(i, j - 1, grid);
+        dfs(i , j + 1, grid);
+    }
+
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int m = grid.length;
+        int n = grid[0].length;
+        int count = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(i, j, grid);
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
